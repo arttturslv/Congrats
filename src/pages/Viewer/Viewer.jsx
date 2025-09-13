b7163fa5e78611c7e8e281bcb410e415770ae52d
arttturslv <arttturslv@gmail.com>
2025-09-13 10:20:42 -0300

    fix: message error

--- src/pages/Viewer/Viewer.jsx
+++ src/pages/Viewer/Viewer.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { useState, useEffect } from "react";
 import { getCard } from "../../hooks/useAPI";
 import { useParams } from "react-router-dom";
@@ -5,14 +7,14 @@ import { Link } from "react-router-dom";
 import Auth from "./Authentication";
 import Introduction from "./Introduction";
 import CardViewer from "./CardViewer";
-import arrowIcon from "../../assets/images/arrow.png"
-import giftBoxIcon from "../../assets/images/gift-box.png"
+import arrowIcon from "../../assets/images/arrow.png";
+import giftBoxIcon from "../../assets/images/gift-box.png";
 
 export default function Viewer() {
   const { id, passKey } = useParams();
 
   const [card, setCard] = useState(null);
-  const [cardStatus, setCardStatus] = useState('null');
+  const [cardStatus, setCardStatus] = useState("null");
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -25,32 +27,33 @@ export default function Viewer() {
       setLoading(true);
 
       try {
-        let {status, res} = await getCard(id, userPassKey);
-        setCardStatus(status); 
-        if(status=="success") {
+        let { status, res } = await getCard(id, userPassKey);
+        setCardStatus(status);
+        if (status == "success") {
           setCard(res);
         }
-
       } catch (error) {
         setError(error);
       } finally {
         setLoading(false);
       }
     };
-    console.log("- ", card)
+    console.log("- ", card);
     if (card === null) {
       fetchData();
     }
   }, [id, userPassKey]);
 
-  
   if (error) {
     return (
-      <div className="space-y-2 text-center flex justify-center items-center h-screen flex-col">
+      <div className="space-y-2 text-center flex justify-center items-center  h-screen flex-col">
+        <h1 className="font-zig text-2xl py-24">
+          Não encontramos a mensagem :(
+        </h1>
         <p className=" text-[#d34444]">{error.message}</p>
 
         <Link
-          className="w-full flex justify-center group items-center gap-2 bg-dark border-redHighlight border-4 px-3 py-1 placeholder:text-sm"
+          className="w-full sm:max-w-[300px] flex justify-center group items-center gap-2 bg-dark border-redHighlight border-4 px-3 py-1 placeholder:text-sm"
           to="/"
         >
           <>
@@ -64,34 +67,34 @@ export default function Viewer() {
         </Link>
       </div>
     );
-  } 
+  }
 
   if (loading) {
     return (
       <div className="space-y-2 text-center flex justify-center items-center h-screen flex-col">
         <img
-              className={`size-32`}
-              src={giftBoxIcon}
-              alt="caixa de presente"
-            ></img>
+          className={`size-32`}
+          src={giftBoxIcon}
+          alt="caixa de presente"
+        ></img>
         <p className=" text-light animate-pulse">Procurando por presentes</p>
       </div>
-    )
+    );
   }
 
-  if (cardStatus=="success") {
-    return (
-      nextPage ?
+  if (cardStatus == "success") {
+    return nextPage ? (
       <CardViewer card={card}></CardViewer>
-      :
-      <Introduction receiver={card.receiverName} setCard={setCard} setShowPage={setNextPage}></Introduction>
-    )
+    ) : (
+      <Introduction
+        receiver={card.receiverName}
+        setCard={setCard}
+        setShowPage={setNextPage}
+      ></Introduction>
+    );
   }
 
-  if (cardStatus=="pending") {
-    return (
-      <Auth setPassKey={setUserPassKey} passKey={userPassKey}></Auth>
-    )
+  if (cardStatus == "pending") {
+    return <Auth setPassKey={setUserPassKey} passKey={userPassKey}></Auth>;
   }
-
 }
